ca2e36240f6dcb0c4c7e29b8d516f8e3e9b5db27
Ravid Efroni <ravidefroni@gmail.com>
2024-09-23 13:30:48 +0200

    Add skeleton of the html of the page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,144 @@ import logo from './logo.svg';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <h3>Jason Briscore</h3>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section>
+          <div>
+            <h1>Design partner for early stage tech and scale ups.</h1>
+          </div>
+          <div>
+            <p>
+              I’ve spent a couple of decades creating brands and digital
+              products — acting as a VP of design, co-founder and advisor. Based
+              in London, Toronto and NYC.
+            </p>
+            <p>
+              My studio <a href="https://theworkshop.co/">The Workshop</a>{' '}
+              partners with founders and VC to brand, build and bring product
+              ideas to life.
+            </p>
+            <a href="mailto:jb@jasonbriscoe.com">Email me</a>
+            <a href="https://calendly.com/">Schedule a discovery call</a>
+          </div>
+        </section>
+        <section>
+          <div>
+            <video>video content goes here</video>
+          </div>
+        </section>
+
+        <section>
+          <h1>
+            Concept.
+            <br />
+            Brand. Build. <br />
+            Launch. Exit.
+          </h1>
+        </section>
+        <section>
+          <div>
+            <video>video content goes here</video>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <p>
+              I help companies make strategic foundational decisions and
+              leverage their position in the market — providing long-term
+              support, often through a blend of cash and equity.
+            </p>
+            <p>
+              In recent years, I've helped build multiple businesses leading to
+              over $55M in capital investment.
+            </p>
+          </div>
+          <div>
+            <h1>
+              Here’s what you can
+              <br /> expect from me...
+            </h1>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <p>Value</p>
+              <p>Experience, big energy and fresh thinking.</p>
+            </div>
+            <div>
+              <p>Jordan Knott, Founder</p>
+              <p>
+                "It’s very rare in life when you meet someone that is so
+                decisively exceptional at what they do."
+              </p>
+            </div>
+            <div>
+              <p>Insights</p>
+              <p>
+                Bridge the gap between creative and commercial objectives —
+                speaking both languages
+              </p>
+            </div>
+            <div>
+              <p>Peter Hwang, Founder</p>
+              <p>
+                "From a creative perspective, I’ve never worked with a more
+                talented individual."
+              </p>
+            </div>
+            <div>
+              <p>Growth</p>
+              <p>
+                The foundations to scale — from world-class design and advisory
+                to building your future team.
+              </p>
+            </div>
+            <div>
+              <p>Fun</p>
+              <p>
+                Teach teams to move at velocity and have a hell of a lot of fun
+                doing it.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <p>
+              Culturally, I fit high-energy and fun management teams.
+              <br />
+              I compete in Hyrox races around the world, care deeply about being
+              a present father and husband, and maintain a healthy balance
+              between work, life and play.
+              <br />
+              Feel free to email me, schedule a call or connect on LinkedIn.
+              <br />
+              Let’s have some fun!
+            </p>
+            <img
+              style={{ width: '100px' }}
+              src="https://gzhls.at/i/49/72/2624972-l0.jpg"
+              alt="signature"
+            />
+          </div>
+        </section>
+        <footer>
+          <ul>
+            <li>Jason Briscoe</li>
+            <li>Linkedin</li>
+            <li>read.cv</li>
+            <li>X</li>
+          </ul>
+        </footer>
+      </main>
+    </>
   );
 }
